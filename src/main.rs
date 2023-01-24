1ec56ded566c7c38a1700631a6cbfab1f8c70aee
Karl Heitmann <khf1988@gmail.com>
2023-01-24 18:16:02 -0300

    added TUI sample

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use serde_yaml; // 0.8.7
 use serde_yaml::Value;
+use crossterm::terminal::enable_raw_mode;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+use crossterm::terminal::disable_raw_mode;
+use std::io;
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let f = std::fs::File::open("en.yml")?;
@@ -10,5 +16,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Read YAML string: {:?}", d);
     // let d = d.unwrap();
+    enable_raw_mode().expect("can run in raw mode");
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    disable_raw_mode()?;
+    terminal.show_cursor()?;
+
     Ok(())
+
 }
